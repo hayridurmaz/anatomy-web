ad1ff605ab4631d58eca059ee66dfc729d7e6d9a
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-15 01:31:18 +0300

    Rest controller added

--- src/main/java/tr/edu/tedu/anatomyweb/Controller/HelloController.java
+++ src/main/java/tr/edu/tedu/anatomyweb/Controller/HelloController.java
@@ -1,15 +1,13 @@
 package tr.edu.tedu.anatomyweb.Controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HelloController  {
 
     @RequestMapping("/")
-    @ResponseBody
     String home() {
         return "Hello World!";
     }
