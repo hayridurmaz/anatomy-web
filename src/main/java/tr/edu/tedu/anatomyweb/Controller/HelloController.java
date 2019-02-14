93aafa8bb596dd4eeee23d99c7f1042fd4a751a1
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-15 01:37:33 +0300

    @CrossOrigin

--- src/main/java/tr/edu/tedu/anatomyweb/Controller/HelloController.java
+++ src/main/java/tr/edu/tedu/anatomyweb/Controller/HelloController.java
@@ -1,9 +1,10 @@
 package tr.edu.tedu.anatomyweb.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 public class HelloController  {
 
