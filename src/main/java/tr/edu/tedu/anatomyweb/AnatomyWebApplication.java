c17075c4744178ee82c40164e5e4ffe17ac633b0
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-15 00:18:16 +0300

    initial commit

---
+++ src/main/java/tr/edu/tedu/anatomyweb/AnatomyWebApplication.java
@@ -0,0 +1,14 @@
+package tr.edu.tedu.anatomyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnatomyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnatomyWebApplication.class, args);
+    }
+
+}
+
