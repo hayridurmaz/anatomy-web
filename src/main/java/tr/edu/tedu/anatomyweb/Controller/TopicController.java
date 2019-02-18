b7148e8c8c90b977287df7cb6b3d18fa390cae78
Arda Tümay <arda.tumay@tedu.edu.tr>
2019-02-19 00:36:41 +0300

    topic model bug fixes

--- src/main/java/tr/edu/tedu/anatomyweb/Controller/TopicController.java
+++ src/main/java/tr/edu/tedu/anatomyweb/Controller/TopicController.java
@@ -4,6 +4,7 @@ package tr.edu.tedu.anatomyweb.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tr.edu.tedu.anatomyweb.Exception.ResourceNotFoundException;
 import tr.edu.tedu.anatomyweb.Model.QUIZ;
 import tr.edu.tedu.anatomyweb.Model.SYSTEM;
 import tr.edu.tedu.anatomyweb.Model.TOPIC;
@@ -27,21 +28,22 @@ public class TopicController {
     }
 
     @PostMapping("/CreateTopic")
-    public SYSTEM createTopic(@Valid @RequestBody SYSTEM system) {
-        return systemService.save(system);
+    public TOPIC createTopic(@Valid @RequestBody TOPIC topic) {
+        return repository.save(topic);
     }
 
     @PutMapping("/UpdateTopic/{TopicId}")
-    public SYSTEM updateSystem(@PathVariable Long SystemId,
-                               @Valid @RequestBody SYSTEM systemRequest) {
-        SYSTEM s = systemService.findById(SystemId);
-        s.setNAME(systemRequest.getNAME());
-        return systemService.save(s);
+    public TOPIC updateSystem(@PathVariable Long TopicId,
+                               @Valid @RequestBody TOPIC topicRequest) {
+        TOPIC t = repository.findById(TopicId).orElseThrow(() -> new ResourceNotFoundException("Quiz not found with id " + TopicId));
+        t.setNAME(topicRequest.getNAME());
+        return repository.save(t);
     }
 
     @DeleteMapping("/DeleteTopic/{TopicId}")
-    public ResponseEntity<?> deleteSystem(@PathVariable Long systemId) {
-        systemService.delete(systemId);
+    public ResponseEntity<?> deleteSystem(@PathVariable Long topicId) {
+        TOPIC t = repository.findById(topicId).orElseThrow(() -> new ResourceNotFoundException("Quiz not found with id " + topicId));;;
+        repository.delete(t);
         return ResponseEntity.ok().build();
 
     }
