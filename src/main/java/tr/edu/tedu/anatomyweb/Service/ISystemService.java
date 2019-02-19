a226336d0f2db1bc58d1ff65faa70153196206f7
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-19 03:02:06 +0300

    code cleaning

--- src/main/java/tr/edu/tedu/anatomyweb/Service/ISystemService.java
+++ src/main/java/tr/edu/tedu/anatomyweb/Service/ISystemService.java
@@ -1,10 +1,8 @@
 package tr.edu.tedu.anatomyweb.Service;
 
-import tr.edu.tedu.anatomyweb.Model.QUIZTYPE;
 import tr.edu.tedu.anatomyweb.Model.SYSTEM;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ISystemService {
 
