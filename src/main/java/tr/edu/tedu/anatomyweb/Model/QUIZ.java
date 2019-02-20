9f17cc047adf004cea3907a0e73126f993da2024
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-02-20 15:15:17 +0300

    exception handled

--- src/main/java/tr/edu/tedu/anatomyweb/Model/QUIZ.java
+++ src/main/java/tr/edu/tedu/anatomyweb/Model/QUIZ.java
@@ -1,7 +1,5 @@
 package tr.edu.tedu.anatomyweb.Model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
 import java.util.List;
 import java.util.Objects;
@@ -17,13 +15,13 @@ public class QUIZ {
     @ManyToOne(optional = false)
     @JoinColumn(name = "quiz_type_id", nullable = false)
     // @OnDelete(action = OnDeleteAction.CASCADE)
-    //@JsonIgnore
+    // @JsonIgnore
     private QUIZTYPE quiztype;
 
     @ManyToOne(/* = FetchType.LAZY */optional = false)
     @JoinColumn(name = "system_id", nullable = false)
     // @OnDelete(action = OnDeleteAction.CASCADE)
-    //@JsonIgnore
+    // @JsonIgnore
     private SYSTEM system;
 
     @OneToMany(mappedBy = "quiz"/* fetch = FetchType.LAZY */)
