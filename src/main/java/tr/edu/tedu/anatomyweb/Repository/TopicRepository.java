6f4ecd0f52d3397826ace2c27cfecf2d793d88d6
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2019-03-07 00:51:18 +0300

    Topic order added

--- src/main/java/tr/edu/tedu/anatomyweb/Repository/TopicRepository.java
+++ src/main/java/tr/edu/tedu/anatomyweb/Repository/TopicRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tr.edu.tedu.anatomyweb.Model.TOPIC;
 
+import java.util.List;
+
 @Repository
 public interface TopicRepository extends JpaRepository<TOPIC, Long> {
+    public List<TOPIC> findAllByOrderByIDAsc();
 }
